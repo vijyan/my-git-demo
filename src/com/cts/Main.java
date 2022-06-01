2852fcd510daa3a024fe7dcee9b284f5266570fa
vijyan <vijyansinghspic@gmail.com>
2022-06-01 21:59:30 +0530

    first branch 'is1' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,9 +1,13 @@
 package com.cts;
 
+import com.cts.service.SomeService;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to github");
 		System.out.println("Hope you are able to follow");
+		SomeService ss=new SomeService();
+		System.out.println(ss.findLength("program"));
 	}
 }
---
+++ src/com/cts/service/SomeService.java
@@ -0,0 +1,8 @@
+package com.cts.service;
+
+public class SomeService {
+
+	public int findLength(String str) {
+		return str.length();
+	}
+}
