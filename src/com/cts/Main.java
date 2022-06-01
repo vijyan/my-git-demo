44a6db7276957c9007e1df89f8bb7b2442b8608c
vijyan <vijyansinghspic@gmail.com>
2022-06-01 20:50:36 +0530

    initial commit

---
+++ src/com/cts/Main.java
@@ -0,0 +1,9 @@
+package com.cts;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to github");
+		System.out.println("Hope you are able to follow");
+	}
+}
